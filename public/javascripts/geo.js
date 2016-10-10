ec1ca1c50e579a6f572894e8e5f14ffd573eb7da
Metel Patel <metel.patel@Metels-Work-Mac.local>
2016-10-10 13:43:47 -0500

    working on shuffling

--- public/javascripts/geo.js
+++ public/javascripts/geo.js
@@ -1,44 +1,66 @@
-function newMap(map, latitude, longitude) {
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+var map;
+var infowindow;
+var shuffled;
+
+function initMap() {
+  var pyrmont = {lat: 30.400199, lng: -97.736919};
+
   map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: latitude, lng: longitude},
-  zoom: 10
+    center: pyrmont,
+    zoom: 15
   });
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: pyrmont,
+    radius: 2000,
+    type: ['restaurant']
+  }, callback);
+}
+
+function shuffle(array) {
+    var counter = array.length, temp, index;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+        shuffled = shuffle(results[i]);
+        createMarker(shuffled);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
-    position: {lat: latitude, lng: longitude},
     map: map,
-    title: 'You are here'
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
   });
 }
-
-function newInfo(latitude, longitude, locAccuracy) {
-  var output = document.getElementById("info");
-  var locAccuracy = Math.round(locAccuracy *= 3.28084);
-  output.innerHTML = "<p>Latitude:<BR>" + latitude + "<BR><BR>Longitude:<BR>" + longitude + 
-  "<BR><BR>Margin of error:<BR>" + locAccuracy + " feet(+/-)</p>";
-}
-
-function success(position) {
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
-  var locAccuracy = position.coords.accuracy;
-  var map;
-  newMap(map, latitude, longitude);
-  newInfo(latitude, longitude, locAccuracy);
-}
-
-function failure(err) {
-  if (err.code == 1) {
-    alert("Your browser is not allowing this site to locate your computer. If you want to change this, refresh the page, click the 'locate' button again, and click 'allow' when your browser asks for your permission.");
-  } else {
-    alert("Your location is unavailable.")
-  }
-}
-
-function locate() {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(success, failure, {timeout:70000}); 
-  } else {
-    alert("Sorry, your browser doesn't support geolocation.");
-    return;
-  }
-};
